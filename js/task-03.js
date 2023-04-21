661a1926b2654ca9ae12d34a0c569c32ff9cabe5
Oleksandra <photoliya@gmail.com>
2023-04-21 23:56:50 +0300

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector('.gallery');
+
+
+const imageElements = images.map(image => {
+ return `<li><img src="${image.url}" alt="${image.alt}" class="image"></li>`;
+}).join("");
+
+galleryList.insertAdjacentHTML("afterbegin", imageElements);
