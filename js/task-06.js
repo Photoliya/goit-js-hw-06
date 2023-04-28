aed6102be131daee96f519b35291dc21195ff3bf
Oleksandra <photoliya@gmail.com>
2023-04-28 22:10:43 +0300

    edit 6, 7

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,9 @@
 const inputEl = document.querySelector('#validation-input');
 
 function onInputBlur(event) {
-    if (event.target.value.length !== Number(event.target.dataset.length)) {
-    event.target.classList.add('invalid');
-    event.target.classList.remove('valid');
+    if (event.target.value.length !== Number(event.currentTarget.dataset.length)) {
+    event.currentTarget.classList.add('invalid');
+    event.currentTarget.classList.remove('valid');
     return;
   }
   event.target.classList.add('valid');
--- js/task-07.js
+++ js/task-07.js
@@ -4,7 +4,7 @@ const inputFontSize = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('#text');
 
 function fontSizeChange(event) {
-  text.style.fontSize = `${event.target.value}px`;
+  text.style.fontSize = `${event.currentTarget.value}px`;
 }
 
 inputFontSize.addEventListener('input', fontSizeChange)
